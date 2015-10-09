a72b5114b20464eaea15c1d3a02ef74540f51dd6
Kurt Bendl <kurt@tool.net>
2015-10-09 16:11:01 -0600

    Starting the capture. All broken!!!!!

--- nportal/views/request_user_account.py
+++ nportal/views/request_user_account.py
@@ -9,26 +9,24 @@ from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
 from pyramid.renderers import get_renderer
 # from sqlalchemy.exc import DBAPIError
-
-import deform
-import deform.widget
-from deform import ZPTRendererFactory
-from deform import Form
-from deform import (widget)  # decorator, default_renderer, field, form,
+#import deform
+from deform import (ZPTRendererFactory,
+                    Form,
+                    widget,
+                    ValidationFailure)
+    # decorator, default_renderer, field, form,
 import colander
 from pkg_resources import resource_filename
 
-resource_registry = deform.widget.ResourceRegistry()
+resource_registry = widget.ResourceRegistry()
 deform_templates = resource_filename('deform', 'templates')
 tpath = os.getcwd()
 search_path = (tpath + '/nportal/templates', deform_templates)
 
 drenderer = ZPTRendererFactory(search_path)
 # Form.set_zpt_renderer(search_path)
-
 # import htmllaundry
 # from htmllaundry import sanitize
-
 
 from nportal.models import (
     DBSession,
@@ -45,63 +43,32 @@ from .lists import (us_states,
                     cyber_policy)
 
 
+def site_layout():
+    renderer = get_renderer("../templates/_layout.pt")
+    layout = renderer.implementation().macros['layout']
+    return layout
+
+
 class AccountRequestView(object):
+    """
+    """
     def __init__(self, request):
         self.request = request
         renderer = get_renderer("../templates/_layout.pt")
-
-        self.layout = renderer.implementation().macros['layout']
+        #self.layout = renderer.implementation().macros['layout']
+        self.layout = site_layout()
         self.title = "Account Request Form"
-
-    def render_form(self, form, appstruct=colander.null,
-                    submitted='submit',
-                    success=None, readonly=False):
-        captured = None
-
-        import pdb; pdb.set_trace()
-
-        if submitted in self.request.POST:
-            # the request represents a form submission
-            controls = self.request.POST.items()
-            try:
-                # try to validate the submitted values
-                captured = form.validate(controls)
-                if success:
-                    response = success()
-                    if response is not None:
-                        return response
-
-                self.title = 'Form data accepted'
-
-                _add_new_user_request(captured)
-
-            except deform.ValidationFailure as e:
-                # the submitted values could not be validated
-                html = e.render()
-                return dict(form=html, captured=repr(captured),
-                            title=self.title)
-
-            # Data should be added by now.
-            # Send the user's browser to the detailed view.
-            view_url = self.request.route_url('request_received_view',
-                                              fname=captured['givenName'])
-            return HTTPFound(view_url)
-        else:
-            return self.add_new_user_account()
 
     @reify
     def account_req_form(self):
-
-        # import pdb; pdb.set_trace()
-
         schema = AddAccountSchema().bind(
             country_codes_data=country_codes,
             us_states_data=us_states
         )
-        return deform.Form(schema, buttons=('submit',))
+        return Form(schema, buttons=('submit',))
 
     @reify
-    def reqts(self):
+    def reqts(self, request):
         return self.account_req_form.get_widget_resources()
 
     @view_config(route_name='request_received_view',
@@ -109,7 +76,6 @@ class AccountRequestView(object):
     def request_received_view(self):
         return {'title': 'request received',
                 'page_title': 'Account request submission received'}
-
 
     @view_config(route_name='request_user_account',
                  renderer='../templates/request_user_account.pt')
@@ -121,10 +87,19 @@ class AccountRequestView(object):
         )
         Form.set_zpt_renderer(search_path)
         form = Form(schema,
-                    renderer=drenderer,
-                    action=self.request.route_url('request_user_account')
+                    action=self.request.route_url('request_user_account'),
+                    id='ESIF Account Registration'
                     )
 
+        if 'submit' in self.request.POST:
+            # it's a submission, process it
+            import pdb; pdb.set_trace()
+            controls = self.request.POST.items()
+
+
+
+
+        # not submitted, render form
         return dict(form=form)
 
 
