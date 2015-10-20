5099535530746d13a673b0bda08ebce22b9ae030
Kurt Bendl <kurt@tool.net>
2015-10-20 15:39:17 -0600

    Populate CountryCodes.

--- nportal/scripts/initializedb.py
+++ nportal/scripts/initializedb.py
@@ -13,9 +13,13 @@ from pyramid.scripts.common import parse_vars
 
 from ..models import (
     DBSession,
-    SiteModel,
     Base,
+    SiteModel,
+    UserRequest,
+    CountryCodes
     )
+
+from .data import country_codes
 
 
 def usage(argv):
@@ -39,3 +43,8 @@ def main(argv=sys.argv):
     with transaction.manager:
         model = SiteModel(name='one', value=1)
         DBSession.add(model)
+
+    with transaction.manager:
+        for i in country_codes:
+            model = CountryCodes(code=i[0], name=i[1])
+            DBSession.add(model)
