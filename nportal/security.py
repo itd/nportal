169e8812cf9177e340da4bb90354eac86a7839e6
Kurt Bendl <kurt@tool.net>
2016-01-22 15:51:06 -0700

    grrr

--- nportal/security.py
+++ nportal/security.py
@@ -52,6 +52,19 @@ class RootFactory(object):
     def __init__(self, request):
         pass
 
+config.ldap_setup(
+'ldap://ldap.example.com',
+bind='CN=ldap user,CN=Users,DC=example,DC=com',
+passwd='ld@pu5er')
+
+
+def includeme(config):
+    """Set up Configurator methods for pyramid_ldap3."""
+    config.add_directive('ldap_setup', ldap_setup)
+    config.add_directive('ldap_set_login_query', ldap_set_login_query)
+    config.add_directive('ldap_set_groups_query', ldap_set_groups_query)
+    return config
+
 
 def groupfinder(userdn, request):
     """Groupfinder function for Pyramid.
